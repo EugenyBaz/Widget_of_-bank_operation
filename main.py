896c52938c600d6e3df93235dca818bb381d523e
Bazavod <eugeny.bazavod@list.ru>
2024-12-04 22:07:39 +0300

    delete some moduls and linters

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.masks import get_mask_card_number, get_mask_account
-
-if __name__ == "__main__":
-    number_card = input("Введите номер карты")
-    print(get_mask_card_number(number_card))
-    number_account = input("Введите номер счета")
-    print(get_mask_account(number_account))
