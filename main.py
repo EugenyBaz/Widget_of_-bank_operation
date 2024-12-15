d7a768021f8deeea4f0259e78e0fc4ed648fcdb5
Bazavod <eugeny.bazavod@list.ru>
2024-12-15 09:30:38 +0300

    add widget func

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from src.widget import mask_account_card, get_date
 from src.processing import filter_by_state, sort_by_date
 
 if __name__ == "__main__":
+    number_card = input("Введите номер карты или счета")
+    print(mask_account_card(number_card))
+
+    date= input("Введите дату")
+    print(get_date(date))
+
+
+if __name__ == "__main__":
+
+
+
     state = input ("Введите статус")
     state_str = state.upper()
 
--- src/processing.py
+++ src/processing.py
@@ -13,6 +13,6 @@ def filter_by_state(list_dict: list[dict[str, Any]], state_str: str = "EXECUTED"
 
 def sort_by_date(list_dict: list[dict[str, Any]], reverse_str: bool = "True") -> list[dict[str, Any]]:
     """Функция сортировки по дате по убыванию"""
-    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse= reverse_str)
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=reverse_str)
 
     return sorted_list
