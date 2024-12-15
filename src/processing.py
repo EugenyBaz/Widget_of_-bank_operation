a73fa39ec8534ba5d85877dcd5d0dad0792d9223
Bazavod <eugeny.bazavod@list.ru>
2024-12-15 08:01:57 +0300

    update according PEP8

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 from typing import Any
 
-def filter_by_state(list_dict: list[dict[str,Any]], state_str: str = "EXECUTED" ) -> list[dict[str,Any]]:
+
+def filter_by_state(list_dict: list[dict[str, Any]], state_str: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция возвращает новый список словарей, у которых ключ state  соответствует определенному значению."""
     new_list = []
 
@@ -10,8 +11,8 @@ def filter_by_state(list_dict: list[dict[str,Any]], state_str: str = "EXECUTED" 
     return new_list
 
 
-def sort_by_date(list_dict: list[dict[str,Any]]) -> list[dict[str,Any]]:
-    """ Функция сортировки по дате по убыванию"""
-    sorted_list = sorted(list_dict, key=lambda x: x['date'], reverse=True)
+def sort_by_date(list_dict: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    """Функция сортировки по дате по убыванию"""
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=True)
 
     return sorted_list
