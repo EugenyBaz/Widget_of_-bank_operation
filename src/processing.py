7615362b93130cd9bb60f4201f03a4510c9589d8
Bazavod <eugeny.bazavod@list.ru>
2024-12-15 07:59:52 +0300

    fixed type, change func

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,16 @@
+from typing import Any
 
-
-def filter_by_state(list_dict: [str | int]) -> [str | int]:
+def filter_by_state(list_dict: list[dict[str,Any]], state_str: str = "EXECUTED" ) -> list[dict[str,Any]]:
     """Функция возвращает новый список словарей, у которых ключ state  соответствует определенному значению."""
-    list_ex = []
-    list_cncl = []
+    new_list = []
+
     for i in list_dict:
-        if i["state"] == "EXECUTED":
-            list_ex.append(i)
-        elif i["state"] == "CANCELED":
-            list_cncl.append(i)
-    return f"{list_ex}\n{list_cncl}"
+        if i["state"] == state_str:
+            new_list.append(i)
+    return new_list
 
 
-def sort_by_date(list_dict: [str | int]) -> [str | int]:
+def sort_by_date(list_dict: list[dict[str,Any]]) -> list[dict[str,Any]]:
     """ Функция сортировки по дате по убыванию"""
     sorted_list = sorted(list_dict, key=lambda x: x['date'], reverse=True)
 
