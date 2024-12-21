b7151f9c31110c6f02eececd5201d9ed09c79b4f
Bazavod <eugeny.bazavod@list.ru>
2024-12-21 23:54:29 +0300

    create negative test for sort

--- src/processing.py
+++ src/processing.py
@@ -7,14 +7,20 @@ def filter_by_state(list_dict: List[Dict[str,Union[str, int]]],
     new_list = []
 
     for i in list_dict:
-        if i["state"] == state_str:
+        if  i["state"] == state_str:
             new_list.append(i)
     return new_list
+
 
 
 def sort_by_date(list_dict: List[Dict[str,Union[str, int]]],
                  reverse_str: bool = True) -> List[Dict[str,Union[str, int]]]:
     """Функция сортировки по дате по убыванию"""
+    for i in list_dict:
+        if i["date"] == "":
+            raise ValueError( "Отсутствует дата")
+
+
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=reverse_str)
 
     return sorted_list
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -113,3 +113,12 @@ def test_sort_by_date(sort_dict, reverse_str,expected):
                     ],
                     True)== expected
 
+
+def test_invalid_sort_by_date(sort_dict):
+    with pytest.raises(ValueError):
+        sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": ""},
+                        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                    ])
+
