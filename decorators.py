58a635f827d7906e67fb0f0c79ac8faa2425ebf5
Bazavod <eugeny.bazavod@list.ru>
2025-01-04 23:12:04 +0300

    update @log

--- decorators.py
+++ decorators.py
@@ -1,13 +1,15 @@
-
-
-import time
+import datetime
 
 def log (filename="mylog.txt"):
 
     def decorator(func):
 
         def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
+            start_time = datetime.datetime.now()
+            result= func(*args, **kwargs)
+            end_time = datetime.datetime.now()
+            print(f"{func.__name__} start: {start_time} and end:{end_time} result:{result}")
+            return result
         return wrapper
 
     return decorator
