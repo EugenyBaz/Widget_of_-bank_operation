415994057722f627532ad04e4996f28d74bf4fb0
Bazavod <eugeny.bazavod@list.ru>
2025-01-22 19:48:42 +0300

    add API_KEY to .env

--- src/external_second_api.py
+++ src/external_second_api.py
@@ -1,17 +1,20 @@
+import os
 import json
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 def convert_currency(currency):
     cur_in = currency
     cur_out = "RUB"
 
-    url = f"https://v6.exchangerate-api.com/v6/2eeb61ae9abeb967454825f0/latest/{cur_in}"
+    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{cur_in}"
 
-
-    headers = {"apikey": "2eeb61ae9abeb967454825f0"}
 
     response = requests.request("GET", url)
-    status_code = response.status_code
     result = response.text
     result_py = json.loads(result)
 
@@ -19,6 +22,5 @@ def convert_currency(currency):
 
     return tot_res
 
-print (convert_currency("EUR"))
 
 
