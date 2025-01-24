26ecf7de2b31933686bdb1161fe05f1f51317225
Bazavod <eugeny.bazavod@list.ru>
2025-01-24 18:35:54 +0300

    add docstrings and raise ValueError test_external_second_api.py

--- tests/test_external_second_api.py
+++ tests/test_external_second_api.py
@@ -11,13 +11,17 @@ API_KEY = os.getenv("API_KEY")
 
 
 def mocked_requests_get(*args: str) -> Union[Mock, None]:
+    """ Функция мокирования запроса для имитации ответа от API"""
     if args[0].endswith("/USD"):
         mock_response = Mock()
         mock_response.json.return_value = {"conversion_rates": {"RUB": 101.1234}}
-    return mock_response
+        return mock_response
+    else:
+        raise ValueError(f"Unexpected URL: {f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{"USD"}"}")
 
 
 @patch("requests.get", side_effect=mocked_requests_get)
 def test_convert_currency(mock_get: MagicMock) -> None:
+    """ Функция проверки конвертации"""
     assert convert_currency("USD") == 101.1234
     mock_get.assert_called_once_with(f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD")
