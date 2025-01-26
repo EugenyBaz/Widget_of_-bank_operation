ef3a34a8de99b0c11c086555adf67988f8f09494
Bazavod <eugeny.bazavod@list.ru>
2025-01-26 23:08:07 +0300

    Readme.md entries are made

--- tests/test_reader_csv_excel.py
+++ tests/test_reader_csv_excel.py
@@ -1,12 +1,11 @@
 import json
 from unittest.mock import patch
-
 import pandas as pd
-
 from src.reader_csv_excel import read_transactions_csv, read_transactions_exl
 
 
 def test_read_transactions_csv() -> None:
+    """ Проверка корректности чтения файлов csv"""
     test_data = [{"column1": "value1", "column2": "value2"}, {"column1": "value3", "column2": "value4"}]
     with patch("src.reader_csv_excel.pd.read_csv") as mock_read_csv:
         """ Мокирование вызова pd.read_csv на возвращаемый объект DataFrame """
@@ -20,6 +19,7 @@ def test_read_transactions_csv() -> None:
 
 
 def test_read_transactions_exl() -> None:
+    """ Проверка корректности чтения файлов excel"""
     test_data = [{"column1": "value1", "column2": "value2"}, {"column1": "value3", "column2": "value4"}]
     with patch("src.reader_csv_excel.pd.read_excel") as mock_read_excel:
         """ Мокирование вызова pd.read_excel на возвращаемый объект DataFrame """
